193670b2df7358270d54ff45c276ca21f763f97f
cosmah <ssekirya.cosma@student.utamu.ac.ug>
2022-12-21 14:27:54 +0300

    A random english word-pair generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 //Copyright 2018 the flutter team. All rights reserved.
-
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/main.dart';
 
@@ -12,14 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
